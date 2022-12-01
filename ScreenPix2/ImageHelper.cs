87557deb482cfc4c05b9817df7ae387921caf214
Kristian Skov <kris701kj@gmail.com>
2022-12-01 19:05:03 +0100

    Add project files.

---
+++ ScreenPix2/ImageHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ScreenPix2
+{
+    internal class ImageHelper
+    {
+        public static Bitmap GetScreenshot()
+        {
+            return GetScreenshot(
+                (int)SystemParameters.VirtualScreenLeft,
+                (int)SystemParameters.VirtualScreenTop,
+                (int)SystemParameters.VirtualScreenWidth,
+                (int)SystemParameters.VirtualScreenHeight);
+        }
+
+        public static Bitmap GetScreenshot(int x, int y, int width, int height)
+        {
+            Bitmap bmp = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(x, y, 0, 0, bmp.Size);
+            }
+            return bmp;
+        }
+
+        public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+    }
+}
---
+++ ScreenPix2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ScreenPix2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMicroseconds(10);
+            _timer.Tick += UpdateImage;
+        }
+
+        private void UpdateImage(object? sender, EventArgs e)
+        {
+            var img = ImageHelper.GetScreenshot(100, 100, 100, 100);
+            ScreenshotImage.Source = ImageHelper.BitmapToImageSource(img);
+            img.Dispose();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+    }
+}
