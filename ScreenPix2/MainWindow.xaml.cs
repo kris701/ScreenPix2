67eed656d7e5ee37aa22840fa610d549eb8c266c
Kristian Skov <kris701kj@gmail.com>
2022-12-01 19:54:01 +0100

    Added so you can move the cursor by a pixel at a time

--- ScreenPix2/MainWindow.xaml.cs
+++ ScreenPix2/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,9 +22,13 @@ namespace ScreenPix2
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
         private DispatcherTimer _timer;
         private int _imgWidth = 25;
         private int _imgHeight = 25;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,24 +40,37 @@ namespace ScreenPix2
         private void UpdateImage(object? sender, EventArgs e)
         {
             var position = MouseHelper.GetCursorPosition();
-            var img = ImageHelper.GetScreenshot((int)position.X - _imgWidth / 2, (int)position.Y - _imgHeight/ 2, _imgWidth, _imgHeight);
-            Point center = new Point(img.Width / 2, img.Height / 2);
+            using (System.Drawing.Bitmap img = ImageHelper.GetScreenshot((int)position.X - _imgWidth / 2, (int)position.Y - _imgHeight / 2, _imgWidth, _imgHeight))
+            {
+                MakeCrosshair(img);
 
+                var pixel = img.GetPixel(img.Width / 2, img.Height / 2);
+                string rgb = $"{pixel.R},{pixel.G},{pixel.B}";
+                string hex = "#" + pixel.R.ToString("X2") + pixel.G.ToString("X2") + pixel.B.ToString("X2");
+
+                ColorCanvas.Background = GetBrushFromHex(hex);
+                RGBColorTextblock.Text = rgb;
+                HexColorTextblock.Text = hex;
+                ScreenshotImage.Source = ImageHelper.BitmapToImageSource(img);
+            }
+        }
+
+        private void MakeCrosshair(System.Drawing.Bitmap img)
+        {
+            Point center = new Point(img.Width / 2, img.Height / 2);
             img.SetPixel((int)center.X - 1, (int)center.Y, System.Drawing.Color.Black);
             img.SetPixel((int)center.X + 1, (int)center.Y, System.Drawing.Color.Black);
             img.SetPixel((int)center.X, (int)center.Y - 1, System.Drawing.Color.Black);
             img.SetPixel((int)center.X, (int)center.Y + 1, System.Drawing.Color.Black);
+        }
 
-            var pixel = img.GetPixel(img.Width / 2, img.Height / 2);
-            string hex = "#" + pixel.R.ToString("X2") + pixel.G.ToString("X2") + pixel.B.ToString("X2");
-
+        private Brush GetBrushFromHex(string hex)
+        {
             BrushConverter bc = new BrushConverter();
-            ColorCanvas.Background = (Brush)bc.ConvertFrom(hex);
-            RGBColorTextblock.Text = $"{pixel.R},{pixel.G},{pixel.B}";
-            HexColorTextblock.Text = hex;
-
-            ScreenshotImage.Source = ImageHelper.BitmapToImageSource(img);
-            img.Dispose();
+            var brush = bc.ConvertFrom(hex);
+            if (brush == null)
+                throw new ArgumentNullException($"Error! Could not convert {hex} into a brush!");
+            return (Brush)brush;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -74,6 +92,26 @@ namespace ScreenPix2
                     StatusBorder.BorderBrush = Brushes.Transparent;
                     _timer.Start();
                 }
+            }
+            else if (e.Key == Key.Up)
+            {
+                var position = MouseHelper.GetCursorPosition();
+                SetCursorPos((int)position.X, (int)position.Y - 1);
+            }
+            else if (e.Key == Key.Down)
+            {
+                var position = MouseHelper.GetCursorPosition();
+                SetCursorPos((int)position.X, (int)position.Y + 1);
+            }
+            else if (e.Key == Key.Left)
+            {
+                var position = MouseHelper.GetCursorPosition();
+                SetCursorPos((int)position.X - 1, (int)position.Y);
+            }
+            else if (e.Key == Key.Right)
+            {
+                var position = MouseHelper.GetCursorPosition();
+                SetCursorPos((int)position.X + 1, (int)position.Y);
             }
         }
     }
